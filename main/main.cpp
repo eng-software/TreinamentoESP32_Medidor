0f4e8878d5c7f80e9899df18d808f2d050d82d67
AleMnd <mendes.alessandro34@gmail.com>
2025-11-08 15:21:44 -0300

    SMP3011
    Usei o código do Henrique, mas no meu projeto irei manter apenas o sensor SMP3011

--- main/main.cpp
+++ main/main.cpp
@@ -13,7 +13,6 @@ extern "C"
 }
 
 #include "cSMP3011.h"
-#include "CBMP280.h"
 #include "CGlobalResources.h"
 
 #define LED_PIN     gpio_num_t::GPIO_NUM_16  
@@ -22,7 +21,6 @@ extern "C"
     PROTOTYPES
 */
 void sensorSMP3011Task(void *pvParameters);
-void sensorBMP280Task(void *pvParameters);
 
 void statusLedTask(void *pvParameters);
 
@@ -30,7 +28,6 @@ void statusLedTask(void *pvParameters);
     VARIABLES
 */
 cSMP3011    SMP3011;
-CBMP280     BMP280;
 SemaphoreHandle_t sensorMutex;
 
 /**
@@ -59,10 +56,9 @@ extern "C" void app_main()
     // SMP3011 Initialization
     //------------------------------------------------ 
     SMP3011.init();
-    BMP280.init();
+    
     sensorMutex = xSemaphoreCreateMutex();
     xTaskCreate(sensorSMP3011Task, "sensorSMP3011Task", 4096, NULL, 1, NULL);
-    xTaskCreate(sensorBMP280Task, "sensorBMP280Task", 4096, NULL, 1, NULL);
 
 
     //------------------------------------------------
@@ -88,28 +84,13 @@ extern "C" void app_main()
     lv_obj_align(lblTemperature, LV_ALIGN_TOP_MID, 0, 0);    
     lv_obj_set_y(lblTemperature, 16);
 
-
-    lv_obj_t *lblPressure2 = lv_label_create(scr);    
-    lv_label_set_text_fmt(lblPressure2, "P: %5.1f", BMP280.getPressure());    
-    lv_obj_set_width(lblPressure2, LCD_H_RES);
-    lv_obj_align(lblPressure2, LV_ALIGN_TOP_MID, 0, 0);    
-    lv_obj_set_y(lblPressure2, 32);
-
-    lv_obj_t *lblTemperature2 = lv_label_create(scr);    
-    lv_label_set_text_fmt(lblTemperature2, "T: %3.0f", BMP280.getTemperature());    
-    lv_obj_set_width(lblTemperature2, LCD_H_RES);
-    lv_obj_align(lblTemperature2, LV_ALIGN_TOP_MID, 0, 0);    
-    lv_obj_set_y(lblTemperature2, 48);    
-
     lvgl_port_unlock();
 
     while(1)
     {
         lvgl_port_lock(portMAX_DELAY);        
         lv_label_set_text_fmt(lblPressure     , "P1: %5.1f kPa", SMP3011.getPressure());    
-        lv_label_set_text_fmt(lblTemperature  , "T1: %3.0f oC" , SMP3011.getTemperature());    
-        lv_label_set_text_fmt(lblPressure2    , "P2: %5.1f kPa", BMP280.getPressure());    
-        lv_label_set_text_fmt(lblTemperature2 , "T2: %3.0f oC" , BMP280.getTemperature());  
+        lv_label_set_text_fmt(lblTemperature  , "T1: %3.0f oC" , SMP3011.getTemperature());     
         lvgl_port_unlock();
         vTaskDelay(100/portTICK_PERIOD_MS);
     }    
@@ -121,17 +102,6 @@ void sensorSMP3011Task(void *pvParameters)
     {
         //xSemaphoreTake( sensorMutex, portMAX_DELAY );
         SMP3011.poll(); 
-        //xSemaphoreGive( sensorMutex );
-        vTaskDelay(1/portTICK_PERIOD_MS);
-    }
-}
-
-void sensorBMP280Task(void *pvParameters) 
-{
-    while(1)
-    {
-        //xSemaphoreTake( sensorMutex, portMAX_DELAY );
-        BMP280.poll();
         //xSemaphoreGive( sensorMutex );
         vTaskDelay(1/portTICK_PERIOD_MS);
     }
